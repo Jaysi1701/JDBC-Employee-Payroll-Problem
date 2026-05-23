39ea153730bc7f1bd68d45d304544ead0644ab21
jaysi1701 <jaysigaur9449@gmail.com>
2026-05-23 12:27:54 +0530

    Implemented JDBC UC3 salary update

--- src/main/java/payroll/Main.java
+++ src/main/java/payroll/Main.java
@@ -1,5 +1,7 @@
 package payroll;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,16 @@ public class Main {
         PayrollDBService service =
                 new PayrollDBService();
 
-        service.connectToDatabase();
+        // UPDATE SALARY
+        service.updateSalary(
+                "Bill",
+                5000000.00
+        );
+
+        // READ DATA
+        List<EmployeePayrollData> employees =
+                service.readData();
+
+        employees.forEach(System.out::println);
     }
 }
--- src/main/java/payroll/PayrollDBService.java
+++ src/main/java/payroll/PayrollDBService.java
@@ -1,8 +1,9 @@
 package payroll;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PayrollDBService {
 
@@ -23,6 +24,7 @@ public class PayrollDBService {
         );
     }
 
+    // UC1 CONNECTION
     public void connectToDatabase() {
 
         try {
@@ -37,6 +39,96 @@ public class PayrollDBService {
 
             System.out.println(
                     e.getMessage());
+        }
+    }
+
+    // UC2 READ DATA
+    public List<EmployeePayrollData> readData() {
+
+        List<EmployeePayrollData> employeeList =
+                new ArrayList<>();
+
+        String sql =
+                "SELECT * FROM employee_payroll";
+
+        try (
+                Connection connection =
+                        getConnection();
+
+                Statement statement =
+                        connection.createStatement();
+
+                ResultSet resultSet =
+                        statement.executeQuery(sql)
+        ) {
+
+            while (resultSet.next()) {
+
+                int id =
+                        resultSet.getInt("id");
+
+                String name =
+                        resultSet.getString("name");
+
+                String gender =
+                        resultSet.getString("gender");
+
+                double salary =
+                        resultSet.getDouble("salary");
+
+                LocalDate startDate =
+                        resultSet.getDate("start")
+                                .toLocalDate();
+
+                EmployeePayrollData employee =
+                        new EmployeePayrollData(
+                                id,
+                                name,
+                                gender,
+                                salary,
+                                startDate
+                        );
+
+                employeeList.add(employee);
+            }
+
+        } catch (SQLException e) {
+
+            System.out.println(e.getMessage());
+        }
+
+        return employeeList;
+    }
+
+    // UC3 UPDATE SALARY
+    public void updateSalary(String name,
+                             double salary) {
+
+        String sql =
+                "UPDATE employee_payroll " +
+                        "SET salary = ? WHERE name = ?";
+
+        try (
+                Connection connection =
+                        getConnection();
+
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(sql)
+        ) {
+
+            preparedStatement.setDouble(1, salary);
+
+            preparedStatement.setString(2, name);
+
+            int rowsAffected =
+                    preparedStatement.executeUpdate();
+
+            System.out.println(
+                    "Rows Updated : " + rowsAffected);
+
+        } catch (SQLException e) {
+
+            System.out.println(e.getMessage());
         }
     }
 }
